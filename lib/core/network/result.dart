c03d9ec4d4570181a610d89566f97fe9b295db9d
Quang Dang <>
2025-11-03 16:42:43 +0900

    Fix bug

--- lib/core/network/result.dart
+++ lib/core/network/result.dart
@@ -5,11 +5,7 @@ class Result {
   const Result({this.data, this.statusCode});
 
   factory Result.fromJson(Map<String, dynamic> json) {
-    return Result(
-      data:
-          json['data'] != null ? Map<String, dynamic>.from(json['data']) : null,
-      statusCode: json['statusCode'] as int?,
-    );
+    return Result(data: json['data'], statusCode: json['statusCode'] as int?);
   }
 
   Map<String, dynamic> toJson() {
